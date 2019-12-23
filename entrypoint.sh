666d80c80cf81f4dc19999b4a792e4d76d5fa4fb
Felix Bartels <felix@host-consultants.de>
2019-12-23 21:07:54 +0100

    remove ls
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,5 @@ cd "${GITHUB_WORKSPACE}" || exit
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-ls -la
-
 git ls-files | xargs eclint check \
 	| reviewdog -efm="%f:%l %m" -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
